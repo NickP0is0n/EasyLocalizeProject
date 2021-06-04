75bbcdce902f9d3685d17a148b38fd0916e7f653
Mykola Chaikovskyi <nickchaywork@gmail.com>
2021-06-04 03:55:27 +0300

    Changed window name and resize ability

--- src/main/kotlin/me/nickp0is0n/easylocalize/main.kt
+++ src/main/kotlin/me/nickp0is0n/easylocalize/main.kt
@@ -2,7 +2,9 @@ import androidx.compose.desktop.Window
 import androidx.compose.material.MaterialTheme
 import me.nickp0is0n.easylocalize.ui.MainWindowView
 
-fun main() = Window {
+fun main() = Window (
+    title = "EasyLocalize 0.0.1 alpha",
+    resizable = false) {
     val currentView = MainWindowView()
     MaterialTheme {
         currentView.MainUI()
