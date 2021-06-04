cddc4ca5199bb53f5275d3f5299bb493a5fc2063
Mykola Chaikovskyi <nickchaywork@gmail.com>
2021-06-04 14:41:07 +0300

    Changed window size

--- src/main/kotlin/me/nickp0is0n/easylocalize/main.kt
+++ src/main/kotlin/me/nickp0is0n/easylocalize/main.kt
@@ -1,10 +1,13 @@
 import androidx.compose.desktop.Window
 import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.unit.IntSize
 import me.nickp0is0n.easylocalize.ui.MainWindowView
 
 fun main() = Window (
     title = "EasyLocalize 0.0.1 alpha",
-    resizable = false) {
+    resizable = false,
+    size = IntSize(780, 450)
+) {
     val currentView = MainWindowView()
     MaterialTheme {
         currentView.MainUI()
