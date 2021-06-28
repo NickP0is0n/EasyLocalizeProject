cfdd4c1c41d3ccf81af462cec02b78bac57f651d
Mykola Chaikovskyi <nickchaywork@gmail.com>
2021-06-28 17:56:37 +0300

    Incremented version number

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "me.nickp0is0n"
-version = "0.0.10"
+version = "0.0.11"
 
 repositories {
     jcenter()
@@ -31,7 +31,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "EasyLocalize"
-            packageVersion = "1.0.10"
+            packageVersion = "1.0.11"
             copyright = "Copyright (c) 2021, Mykola Chaikovskyi"
             macOS {
                 bundleID = "me.nickp0is0n.easylocalize"
--- src/main/kotlin/me/nickp0is0n/easylocalize/main.kt
+++ src/main/kotlin/me/nickp0is0n/easylocalize/main.kt
@@ -4,7 +4,7 @@ import androidx.compose.ui.unit.IntSize
 import me.nickp0is0n.easylocalize.ui.MainWindowView
 
 fun main() = Window (
-    title = "EasyLocalize 0.0.10 alpha",
+    title = "EasyLocalize 0.0.11 alpha",
     resizable = false,
     size = IntSize(780, 455)
 ) {
