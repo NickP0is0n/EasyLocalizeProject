65bb898def73e2bfda0f7ca15ca445d04fcb802b
Mykola Chaikovskyi <nickchaywork@gmail.com>
2021-08-01 20:41:48 +0300

    Removing internal build text

--- src/main/kotlin/me/nickp0is0n/easylocalize/utils/AppInfo.kt
+++ src/main/kotlin/me/nickp0is0n/easylocalize/utils/AppInfo.kt
@@ -1,5 +1,5 @@
 package me.nickp0is0n.easylocalize.utils
 
 object AppInfo {
-    val windowTitle = "EasyLocalize 0.1.0 Beta Internal testing build"
+    val windowTitle = "EasyLocalize 0.1.0 Beta"
 }
